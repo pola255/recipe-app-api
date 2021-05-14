a9f9a17489763b61a53db59eeddfee7f55e7f5d5
Paula <paulette255@gmail.com>
2021-05-14 16:41:14 +0100

    Added manage user endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,13 +6,9 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
-        """ Test creating a new user with an email is successful """
         email = 'test@gmail.com'
         password = 'Testpass123'
-        user = get_user_model().objects.create_user(
-        email=email,
-        password=password
-        )
+        user = get_user_model().objects.create_user(email, password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
