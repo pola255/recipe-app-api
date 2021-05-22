3e44da969467005601b696b70aef532e3fed2499
Paula <paulette255@gmail.com>
2021-05-22 13:09:13 +0100

    Added feature for filtering tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -50,18 +50,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
-        """Retrieve the recipes for the authenticated user"""
-        tags = self.request.query_params.get('tags')
-        ingredients = self.request.query_params.get('ingredients')
+        """Return objects for the current authenticated user only"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
         queryset = self.queryset
-        if tags:
-            tag_ids = self._params_to_ints(tags)
-            queryset = queryset.filter(tags__id__in=tag_ids)
-        if ingredients:
-            ingredient_ids = self._params_to_ints(ingredients)
-            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
 
-        return queryset.filter(user=self.request.user)
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
 
     def get_serializer_class(self):
         """Return appropriate serializer class"""
